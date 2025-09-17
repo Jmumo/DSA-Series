e9c7495d2cdc96cd51438dfc775a1d7213608dc3
Jmumo <48655696+Jmumo@users.noreply.github.com>
2025-09-17 09:49:58 +0300

    Create Uniqueproduct.java

---
+++ Uniqueproduct.java
@@ -0,0 +1,6 @@
+Unique product Write a method that, efficiently with respect to time and space used,
+finds the first product in an array that occurs only once in that array. 
+If there are no unique products in the array, null should be returned. For example, 
+firstUniqueProduct(new String[] { "Apple", "Computer", "Apple", "Bag" }) should return "Computer".
+
+
