72975fa77daeb291d55b9cc09d9d3d5e88812c89
Jmumo <48655696+Jmumo@users.noreply.github.com>
2025-09-19 12:11:27 +0300

    Create LeastNumberofUniqueIntegersafterKRemovals.java

---
+++ LeastNumberofUniqueIntegersafterKRemovals.java
@@ -0,0 +1,40 @@
+// Given an array of integers arr and an integer k. Find the least number of unique integers after removing exactly k elements.
+
+ 
+
+// Example 1:
+
+// Input: arr = [5,5,4], k = 1
+// Output: 1
+// Explanation: Remove the single 4, only 5 is left.
+// Example 2:
+// Input: arr = [4,3,1,1,3,3,2], k = 3
+// Output: 2
+// Explanation: Remove 4, 2 and either one of the two 1s or three 3s. 1 and 3 will be left.
+
+
+
+
+class Solution {
+    public int findLeastNumOfUniqueInts(int[] arr, int k) {
+        HashMap<Integer, Integer> freq = new HashMap<>();
+
+        for (int num : arr){
+            freq.put(num, freq.getOrDefault(num, 0) + 1);
+        }
+
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>(freq.values());
+
+        while( k > 0 && !minHeap.isEmpty() ){
+            int freqStore = minHeap.poll();
+
+            if (freqStore <= k){
+                k-=freqStore;    // remove this entire number if its frequency is smaller than k
+            }else{
+                minHeap.offer(freqStore -k);  // remove the number partially if the frequency is larger than k
+                k = 0;
+            }
+        }
+        return minHeap.size();
+    }
+}
